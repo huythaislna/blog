1adc15773ad191e85b597304f9d8bb93924f8673
Tides <huythaislna@gmail.com>
2020-08-14 21:45:39 +0700

    update templates and slug

--- posts/urls.py
+++ posts/urls.py
@@ -9,9 +9,9 @@ from posts.views import (
 
 urlpatterns = [
     path('', posts_home, name='home'),
+    path('posts/create/', PostCreateView.as_view(), name='post-update'),
     path('posts/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/<slug:slug>/edit', update_view, name='post-update'),
-    path('posts/create/', PostCreateView.as_view(), name='post-update'),
     path('security/', category_view, name='category'),
     path('login/', login_view, name="user-login"),
     path('register/', register_view, name="user-register"),
